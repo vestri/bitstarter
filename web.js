5d039fc64ba9143fcb012c0c03600736ad0493a3
Christophe Vestri <vestri@gmail.com>
2013-07-09 00:04:49 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var buf = new Buffer(100);
-buf.copy(fs.readFileSync("index.html"));
+var buf = new Buffer(fs.readFileSync("index.html"));
 
 var app = express.createServer(express.logger());
 
